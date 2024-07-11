42aa35e127fa96f35c9ce4acb5c5b38c837f8a5d
Dániel Buga <bugadani@gmail.com>
2024-07-11 16:29:18 +0200

    Add nrf52833

--- build.rs
+++ build.rs
@@ -1,19 +1,20 @@
 use std::{env, fs::File, io::Write, path::PathBuf};
 
+#[cfg(feature = "nrf51422")]
+const MEM_X: &[u8] = include_bytes!("memory-nrf51.x");
+#[cfg(feature = "nrf52832")]
+const MEM_X: &[u8] = include_bytes!("memory-nrf52.x");
+#[cfg(feature = "nrf52833")]
+const MEM_X: &[u8] = include_bytes!("memory-nrf52833.x");
+
 fn main() {
     // Put `memory.x` in our output directory and ensure it's
     // on the linker search path.
 
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    #[cfg(feature = "nrf51422")]
     File::create(out.join("memory.x"))
         .unwrap()
-        .write_all(include_bytes!("memory-nrf51.x"))
-        .unwrap();
-    #[cfg(feature = "nrf52832")]
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory-nrf52.x"))
+        .write_all(MEM_X)
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
 
